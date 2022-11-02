53fe870f296cbb017f12b49635dd00f9f349b158
NEDGE3 <739516@student.dusd.net>
2022-11-02 09:28:41 -0700

    Finished Loops
    Completed all 4 lopping scripts.

--- ScriptingProjectEdge/Assets/Scripts/DoWhileLoop1.cs
+++ ScriptingProjectEdge/Assets/Scripts/DoWhileLoop1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoWhileLoop : MonoBehaviour
+public class DoWhileLoop1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class DoWhileLoop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
