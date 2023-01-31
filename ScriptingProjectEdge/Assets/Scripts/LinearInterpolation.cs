81ca937c570e52bf0b6fb4bfde37d49424a5460c
NEDGE3 <739516@student.dusd.net>
2023-01-31 09:33:16 -0800

    added script
    added linear interpolation script

---
+++ ScriptingProjectEdge/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    // In this case, result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    //Mathf.Lerp takes 3 float parameters one representing the value to interpolate from
+    //one representing the value to interpolate
+    //one represents how far to interpolate
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    //Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    //Vector3.Lerp finds a vector that is some percentage of the way between the two vectors
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f); 
+    //gradually increases light intensity, rate of change slows
+
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime); 
+    //increses light intensity every second
+    }
+    */
+}
